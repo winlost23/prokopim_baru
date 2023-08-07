1acb304818544c789678eb90dcdc219f29bd42f2
winlost23 <winlost23@gmail.com>
2023-08-07 14:01:30 +0700

    fix auth

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -21,6 +21,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'auth' => \App\Filters\Auth::class
     ];
 
     /**
@@ -60,5 +61,9 @@ class Filters extends BaseConfig
      * Example:
      * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
      */
-    public array $filters = [];
+    public array $filters = [
+        'auth' => [
+            'before' => ['nimda*'],
+        ]
+    ];
 }
